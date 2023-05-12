0c6bcae47d6a7d9cc9a66b199be2a94b37055533
ondrejskacel <ondrejskacel42@gmail.com>
2023-05-12 16:56:23 +0200

    Make the example in main more realistic

--- main.py
+++ main.py
@@ -36,33 +36,33 @@ if __name__ == '__main__':
 
     energies = np.array([8400])
 
-    nickel = {}
+    nickel = {'top': np.zeros(10000),
+              'bottom': np.zeros(10000),
+              'original': np.zeros(10000)}
 
-    nickel['top'] = np.zeros(10000)
     nickel['top'][:500] = 1.0
-
-    nickel['bottom'] = np.zeros(10000)
     nickel['bottom'][-500:] = 1.0
 
     nickel['uniform'] = np.ones(10000) * 500 / 10000
 
-    nickel['original'] = np.ones(10000)
-    nickel['original'][int(4 / 108) * 10000] = 12.0
-    nickel['original'][int(54 / 108) * 10000] = 12.0
-    nickel['original'][int(104 / 108) * 10000] = 11.0
+    nickel['original'][int(4 / 108 * 10000)] = 12.0
+    nickel['original'][int(54 / 108 * 10000)] = 12.0
+    nickel['original'][int(104 / 108 * 10000)] = 11.0
 
     results = {name: output_intensity(1.0, theta_in, theta_out, cell, energies, 7480, density) for name, density in nickel.items()}
 
     # real data
     from experiment_setup import CELLS_FIRST_BATCH
-    experiment = CELLS_FIRST_BATCH[5]
 
-    data = load_experiment_data(experiment)
-    results['real'] = data.values[777-10:777+10, :5].mean(axis=0).reshape((1, -1))  # closest to 8400 ev
+    for cell in CELLS_FIRST_BATCH[3:]:
+
+        data = load_experiment_data(cell)
+        # Band of energies close to 8400 ev
+        results[f'{cell.name}{cell.output_suffix}'] = data.values[777-10:777+10, :5].mean(axis=0).reshape((1, -1))
 
     fig, ax = plt.subplots()
     for name, result in results.items():
-        ax.scatter(range(1, 6), result[0, :] / result[0, 0], label=name)
+        ax.plot(range(1, 6), result[0, :] / result[0, 0], label=name)
 
     plt.legend()
     plt.show()
