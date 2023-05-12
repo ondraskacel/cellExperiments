b7d3553484f7ef3765bb6ab975021ef5a0329dbd
ondrejskacel <ondrejskacel42@gmail.com>
2023-05-12 13:15:24 +0200

    [main] Add a simple example

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from config import Layer, Cell
-from data import load_default_setup
+from data import load_default_setup, load_experiment_data
 from intensity import output_intensity
 
 
@@ -24,24 +24,61 @@ if __name__ == '__main__':
 
     attn_data = load_default_setup()
 
-    anode = Layer(densities={'Pt': 0.1975, 'C': 0.960, 'F': 0.421, 'O': 0.048, 'S': 0.019},
-                  depth=4.0,
-                  attn_data=attn_data)
+    anode = Layer(depth=4.0,
+                  attn_data=attn_data,
+                  densities={'Pt': 0.1975, 'C': 0.960, 'F': 0.421, 'O': 0.048, 'S': 0.019})
+
+    gold_density = 19.3
+    gold = Layer(depth=14.0 / gold_density * 1e-2,  # conversion cm -> m
+                 attn_data=attn_data,
+                 densities={'Au': gold_density})
+
+    nafion = Layer(depth=50.0,
+                   attn_data=attn_data,
+                   formula={'H': 1, 'C': 9, 'O': 5, 'F': 17, 'S': 1},
+                   density=1.9)
 
     layers = [
         anode,
-        Layer(densities={'Au': 20.0}, depth=0.02, attn_data=attn_data),
+        # gold,
+        nafion,
+        # gold,
+        nafion,
+        # gold,
         anode,
     ]
 
     cell = Cell(layers)
 
-    energies = np.linspace(7400.0, 13000.0, 1000)
-    nickel_density = np.linspace(0.0, 1.0, 25)
-    results = output_intensity(1.0, theta_in, theta_out, cell, energies, 7480, nickel_density)
+    energies = np.array([8400])
 
-    for i, theta_out in enumerate(theta_out):
-        plt.plot(energies, results[:, i], label=f'{180 / np.pi * theta_out:.1f} deg')
+    nickel = {}
+
+    nickel['top'] = np.zeros(10000)
+    nickel['top'][:500] = 1.0
+
+    nickel['bottom'] = np.zeros(10000)
+    nickel['bottom'][-500:] = 1.0
+
+    nickel['uniform'] = np.ones(10000) * 500 / 10000
+
+    nickel['original'] = np.ones(10000)
+    nickel['original'][int(4 / 108) * 10000] = 12.0
+    nickel['original'][int(54 / 108) * 10000] = 12.0
+    nickel['original'][int(104 / 108) * 10000] = 11.0
+
+    results = {name: output_intensity(1.0, theta_in, theta_out, cell, energies, 7480, density) for name, density in nickel.items()}
+
+    # real data
+    experiment = 'Ref-CCM-Naf212-Ni-Au-I_a'
+    suffix = '_new_y_1'
+
+    data = load_experiment_data(experiment, suffix)
+    results['real'] = data.values[777-10:777+10, :5].mean(axis=0).reshape((1, -1))  # closest to 8400 ev
+
+    fig, ax = plt.subplots()
+    for name, result in results.items():
+        ax.scatter(range(1, 6), result[0, :] / result[0, 0], label=name)
 
     plt.legend()
     plt.show()
