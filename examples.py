4655ab920df2b6f505bb78a132755346d9ed3f06
ondrejskacel <ondrejskacel42@gmail.com>
2023-05-13 16:52:02 +0200

    Add 'fit_reference_spectra' example

--- examples.py
+++ examples.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import minimize, nnls
+from scipy.special import sh_legendre
 
-from experiment_setup import CELLS_FIRST_BATCH, CELLS_SECOND_BATCH, PELLETS_SECOND_BATCH
+from experiment_setup import CELLS_FIRST_BATCH, CELLS_SECOND_BATCH, PELLETS_SECOND_BATCH, PELLETS_FIRST_BATCH
 from materials import Layer, Cell, ANODE, NAFION
 from data import load_experiment_data
 from intensity import output_intensity
@@ -97,4 +99,59 @@ def run_ls_04_ccm_comparison():
         ax.plot(range(1, 6), result[0, :] / result[0, 0], label=name)
 
     plt.legend()
+    plt.show()
+
+
+def get_spectra(theta, n_energies):
+
+    importances = np.zeros(n_energies)
+    importances[320:] = 0.25
+    importances[170:320] = 0.25
+    importances[110:170] = 0.5
+    importances[70:110] = 1.0
+    importances[:70] = 0.01
+
+    cumsum = np.cumsum(importances)
+    x_grid = cumsum / cumsum[-1]
+
+    n_spectra, n_pol = theta.shape
+
+    x = np.array([sh_legendre(i)(x_grid) for i in range(1, n_pol+1)]).T
+    return np.exp(x @ theta.T)
+
+
+def loss(Y, theta, n_spectra):
+
+    n_energies, n_pellets = Y.shape
+
+    S = get_spectra(theta.reshape((n_spectra, -1)), n_energies)
+    S_ = np.ones((n_energies, n_spectra + 1))
+    S_[:, :-1] = S
+
+    total_loss = 0.0
+    for j in range(n_pellets):
+        c, err = nnls(S_ / np.abs(Y[:, j, None]), Y[:, j] / np.abs(Y[:, j]), maxiter=1000000)
+        total_loss += err
+
+    print(total_loss)
+    return total_loss
+
+
+def fit_reference_spectra():
+
+    pellets = PELLETS_FIRST_BATCH + PELLETS_SECOND_BATCH
+    data = {pellet.name: load_experiment_data(pellet) for pellet in pellets}
+
+    Y = np.array([df['intensity_total'].values for df in data.values()]).T
+    n_spectra = 3
+    n_pol = 5
+
+    res = minimize(lambda theta: loss(Y, theta, n_spectra),
+                   x0=np.random.normal(size=n_spectra * n_pol)*0.1,
+                   method='L-BFGS-B', jac='2-point')
+
+    spectra = get_spectra(res['x'].reshape((n_spectra, -1)), Y.shape[0])
+    for j in range(n_spectra):
+        plt.plot(spectra[:, j] / np.max(spectra[:, j]))
+
     plt.show()
