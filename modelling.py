e7ce3a17dfdd2c39e2daa2250ca1485e754a653d
ondrejskacel <ondrejskacel42@gmail.com>
2023-05-15 12:58:22 +0200

    Clean up fitting, compute errors

--- modelling.py
+++ modelling.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import nnls
 
@@ -16,62 +15,7 @@ def get_density(beta: np.ndarray):
     return np.exp(log_density)
 
 
-def fit_nickel_spectra(df, references, detector, plot=True):
-
-    spectra = {name: spectrum(df['energy']) for name, spectrum in references.items()}
-    target = df[f'intensity_{detector}'].values
-
-    coefficients = {}
-    error_variance = {}
-
-    # Fit background
-    mask_background = (df['energy'] < NICKEL_REGIMES['pre_edge'])
-    background = target[mask_background.values]  # noqa
-    coefficients['background'] = background.mean()
-    error_variance['background'] = background.var()
-
-    residual = target - coefficients['background']
-
-    # Fit the 'metallic' part of the spectrum
-    mask_metallic = (df['energy'] < NICKEL_REGIMES['mixed']) & (df['energy'] > NICKEL_REGIMES['metallic'])
-
-    residual_metallic = residual[mask_metallic.values]  # noqa
-    metallic_reference = spectra['Ni-metallic'][mask_metallic.values]  # noqa
-
-    # Linear regression
-    coef, errors = nonnegative_linear_model(metallic_reference[:, None], residual_metallic)
-    coefficients['Ni-metallic'] = coef[0]
-    error_variance['Ni-metallic'] = errors[0, 0]
-
-    residual = residual - coefficients['Ni-metallic'] * spectra['Ni-metallic']
-
-    # Fit NiO and NiSO4
-    X = np.array([spectra[name] for name in ['NiO', 'NiSO4']]).T
-    coefs, errors = nonnegative_linear_model(X, residual)
-
-    for i, name in enumerate(['NiO', 'NiSO4']):
-        coefficients[name] = coefs[i]
-        error_variance[name] = errors[i, i]
-
-    if plot:
-        plt.plot(df['energy'], target, label='data')
-
-        fit = np.zeros_like(df['energy'])
-        for name, spectrum in spectra.items():
-            contribution = spectrum * coefficients[name]
-            fit += contribution
-            plt.plot(df['energy'], contribution, label=name)
-
-        fit += coefficients['background']
-
-        plt.plot(df['energy'], fit, label='fit')
-        plt.legend()
-        plt.show()
-
-    return coefficients
-
-
-def fit_nickel_spectra_lasso(df, references, detector, fit_nio=False, ax=None):
+def fit_nickel_spectra(df, references, detector, fit_nio=False, ax=None):
 
     names = [name for name in references.keys()]
     if not fit_nio:
@@ -92,7 +36,7 @@ def fit_nickel_spectra_lasso(df, references, detector, fit_nio=False, ax=None):
 
     residual = target - coefficients['background']
 
-    # Fit the rest
+    # Fit the rest - errors are computed independently of the background fitting error (correct in practice)
     X = np.array([spectrum for spectrum in spectra.values()]).T
     coefs, errors = nonnegative_linear_model(X, residual)
 
@@ -109,7 +53,7 @@ def fit_nickel_spectra_lasso(df, references, detector, fit_nio=False, ax=None):
         spectra['background'] = np.ones_like(df['energy'])
 
         colors = ['red', 'green', 'blue', 'black']
-        for i, name in enumerate(names):
+        for i, name in enumerate(names + ['background']):
 
             contribution = coefficients[name] * spectra[name]
             std = np.sqrt(error_variance[name]) * spectra[name]
@@ -129,12 +73,23 @@ def nonnegative_linear_model(x, y):
 
     beta, rse = nnls(x, y)
 
-    # TODO: handle zeros in error computation
+    # Handle zeros for beta = 0.9 in error computation
+    # We set the errors for them to 0 and compute the others as if the model had only the non_zero regressors
+    mask_zero = np.isclose(beta, 0.0)
+    xtx = x.T @ x
+
+    # Make the correlation matrix block-wise (numerical trick)
+    xtx[mask_zero, :] = 0.0
+    xtx[:, mask_zero] = 0.0
+    np.fill_diagonal(xtx, np.where(mask_zero, 1.0, np.diag(xtx)))
 
     # Error computation
-    dof = len(x) - x.shape[1]
-    xtx_inv = np.linalg.inv(x.T @ x)
+    dof = len(x) - np.sum(~mask_zero)
+    xtx_inv = np.linalg.inv(xtx)
 
     errors = np.square(rse) / dof * xtx_inv
+
+    # Zero out errors where beta = 0.0
+    np.fill_diagonal(errors, np.where(mask_zero, 0.0, np.diag(errors)))
 
     return beta, errors
