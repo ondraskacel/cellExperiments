2b8e267c51b7ba35f40d5008a3b07203885ff967
ondrejskacel <ondrejskacel42@gmail.com>
2023-05-16 21:12:47 +0200

    Fix and refactor geometric factor computations

--- geometry.py
+++ geometry.py
@@ -25,7 +25,7 @@ def get_geometric_factor_ascan(plot=False):
     }
 
     coefficients = {}
-    for scan, (number, range) in scans.items():
+    for name, (number, scan_range) in scans.items():
         data = np.load(f'data/geometry_data/scan_{number}.npy')
 
         # Plot detector receptive field
@@ -36,15 +36,17 @@ def get_geometric_factor_ascan(plot=False):
         # plt.show()
 
         crystal_data = [data[:, limits_x[0]:limits_x[1], limit_y[0]: limit_y[1]] for limit_y in limits_y]
+
+        # Intensities per crystal and scan
         intensities = np.array([np.sum(crystal, axis=(1, 2)) for crystal in crystal_data]).T
 
         if plot:
             plt.plot(intensities)
             plt.show()
 
-        intensities = intensities[range[0]:range[1] + 1]
-        coefficients[scan] = intensities.sum(axis=0)
-        coefficients[scan] /= coefficients[scan][0]
+        intensities = intensities[scan_range[0]:scan_range[1] + 1]
+        coefficients[name] = intensities.sum(axis=0)
+        coefficients[name] /= coefficients[name][0]
 
     return coefficients
 
@@ -65,7 +67,7 @@ def get_geometric_factors_energy_scans():
     cells = {'in_cell': load_experiment_data(NI_FOIL[0]),
              'out_of_cell': load_experiment_data(NI_FOIL[1])}
 
-    detectors = [1, 2, 3, 4, 5]
+    detectors = NI_FOIL[0].detectors
 
     norms = {}
     for name, cell in cells.items():
@@ -80,7 +82,7 @@ def get_theoretical_angle_distribution():
     ni_foil = Cell(layers=[KAPTON, Layer(depth=5.0, densities={'Ni': 8.908})])
 
     density = np.zeros(7000)
-    density[6500:] = 1.0
+    density[6500:] = 1.0  # 65 um of no nickel and 5um of nickel only
 
     theta_in = np.pi / 180 * 31
     theta_out = np.pi / 180 * np.array([41, 34, 27, 20, 13])  # Detectors C1-5
