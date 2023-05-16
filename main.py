11644f03b546157ff7d9bcda666b8ee123df0092
ondrejskacel <ondrejskacel42@gmail.com>
2023-05-16 05:11:01 +0200

    Refactor plotting in main

--- main.py
+++ main.py
@@ -7,24 +7,22 @@ from geometry import get_geometric_factors
 from modelling import fit_experiments
 
 
-if __name__ == '__main__':
-
-    cells = PELLETS_FIRST_BATCH
-    coefficients = fit_experiments(cells, 1)
+def plot_experiments(experiments, coefficients, geometric_correction=None):
 
     first_coef = next(iter(coefficients.values()))
     reference_names = [name for name in first_coef if name != 'background']
 
     colors = ['red', 'green', 'blue']
 
-    factors = get_geometric_factors()
-    correction = factors['theoretical'] / factors['in_cell']
+    if geometric_correction is not None:
+        factors = get_geometric_factors()
+        correction = factors['theoretical'] / factors[geometric_correction]
 
-    fig, ax = plt.subplots(2, len(cells))
+    fig, ax = plt.subplots(2, len(experiments))
     coef_y_limit = 0.0
     ratio_y_limits = [np.inf, 0]
 
-    for i, experiment in enumerate(cells):
+    for i, experiment in enumerate(experiments):
 
         name = experiment.output_name or experiment.name
         detectors = experiment.detectors
@@ -32,8 +30,11 @@ if __name__ == '__main__':
 
         coefs_run = {}
         for j, reference in enumerate(reference_names):
+            coefs_run[reference] = np.array([coefficients[(name, detector)][reference] for detector in detectors])
 
-            coefs_run[reference] = np.array([coefficients[(name, detector)][reference] for detector in detectors])  # * correction
+            if geometric_correction is not None:
+                coefs_run[reference] *= correction
+
             ax[0][i].scatter(x_axis, coefs_run[reference], label=reference, color=colors[j])
 
             coef_y_limit = max(coef_y_limit, np.max(coefs_run[reference]))
@@ -46,8 +47,17 @@ if __name__ == '__main__':
         ratio_y_limits[0] = min(ratio_y_limits[0], np.min(ratio))
         ratio_y_limits[1] = max(ratio_y_limits[1], np.max(ratio))
 
-    for i in range(len(cells)):
+    for i in range(len(experiments)):
         ax[0][i].set_ylim([0.0, coef_y_limit])
         ax[1][i].set_ylim(ratio_y_limits)
 
     plt.show()
+
+
+if __name__ == '__main__':
+
+    cells_ = CELL_R
+    fit = fit_experiments(cells_, 3)
+
+    plot_experiments(cells_, fit)
+
