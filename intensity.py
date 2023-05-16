ab48fb79f2f15071ec7f5f63fb0ae2b8f7c9677d
ondrejskacel <ondrejskacel42@gmail.com>
2023-05-16 05:18:27 +0200

    Use np.trapz instead of simple_integration

--- intensity.py
+++ intensity.py
@@ -5,18 +5,6 @@ import numpy as np
 from materials import Cell
 
 
-def simple_integration(x: np.ndarray, low=0.0, high=1.0) -> Union[float, np.ndarray]:
-    """
-    Performs a simple '(1 + 2 + 2 + ... + 2 + 1) / norm' integration over the first axis of x
-    """
-
-    assert len(x) > 2
-
-    norm = len(x) - 1
-    total = np.sum(x, axis=0) - 0.5 * (x[0] + x[-1])
-    return total / norm * (high - low)
-
-
 def output_intensity(
         input_intensity: float,
         theta_in: float,
@@ -44,4 +32,4 @@ def output_intensity(
 
     # axis 0 <-> depth; axis 1 <-> energy_in; axis 2 <-> theta_out
     integrand = nickel_density[:, None, None] * decay_in[:, :, None] * decay_out[:, None, :]
-    return simple_integration(multiplier * integrand, high=cell.total_depth)
+    return np.trapz(multiplier * integrand, depth_grid, axis=0)
